adc3c31d01bded1e16c06d133ce3926d589b6a78
Christina Loiacono <loiaconochristina@gmail.com>
2022-01-28 16:02:01 -0500

    added async/await after importing getAllBookmarks from queries

--- backend/controllers/bookmarkController.js
+++ backend/controllers/bookmarkController.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const bookmarks = express.Router();
 
+// import from queries
+const { getAllBookmarks } = require("../queries/bookmarks.js");
+
 // INDEX
-bookmarks.get("/", (req, res)=>{
-    res.json({ status: "ok" });
+bookmarks.get("/", async (req, res)=>{
+    // res.json({ status: "ok" });
+    const allBookmarks = await getAllBookmarks();
+
+    if (allBookmarks[0]) {
+        res.status(200).json(allBookmarks);
+    } else {
+        res.status(500).json({ error: "server error" });
+    }
 })
 
 module.exports = bookmarks;
