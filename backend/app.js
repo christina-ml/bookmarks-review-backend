523bc2a73dbe9a3d000931b0983a2300e7242b60
Christina Loiacono <loiaconochristina@gmail.com>
2022-01-28 15:03:32 -0500

    set up bookmarkController

--- backend/app.js
+++ backend/app.js
@@ -2,17 +2,26 @@
 const cors = require('cors');
 const express = require('express');
 
+// Bookmarks ROUTES
+const bookmarkController = require("./controllers/bookmarkController.js");
+
 // Configuration
 const app = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use("/bookmarks", bookmarkController);
 
 // Routes
 app.get("/", (req, res)=>{
     res.send("Welcome to Bookmarks App 2.0");
 })
+
+// 404 PAGE
+app.get("*", (req, res) => {
+    res.status(404).send("Page not found");
+  });
 
 // Export
 module.exports = app;
