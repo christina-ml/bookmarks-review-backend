0d28cf0fd04551f05e3fe9ed78c559a69ce0e998
Christina Loiacono <loiaconochristina@gmail.com>
2022-02-01 12:00:14 -0500

    adding createBookmark into the bookmarkController. Added INDEX and SHOW so far.

--- backend/controllers/bookmarkController.js
+++ backend/controllers/bookmarkController.js
@@ -1,17 +1,37 @@
 const express = require("express");
 const bookmarks = express.Router();
 // import from queries
-const { getAllBookmarks } = require("../queries/bookmarks.js");
+const { getAllBookmarks, getBookmark, createBookmark } = require("../queries/bookmarks.js");
 
 // INDEX
 bookmarks.get("/", async (req, res)=>{
-    // res.json({ status: "ok" });
-    const allBookmarks = await getAllBookmarks();
+    try {
+        // res.json({ status: "ok" });
+        const allBookmarks = await getAllBookmarks();
+    
+        if (allBookmarks[0]) {
+            res.status(200).json(allBookmarks);
+        } else {
+            res.status(500).json({ error: "server error" });
+        }
+    } catch (err) {
+        console.log(err);
+    }
+})
 
-    if (allBookmarks[0]) {
-        res.status(200).json(allBookmarks);
-    } else {
-        res.status(500).json({ error: "server error" });
+// SHOW
+bookmarks.get("/:id", async (req, res)=>{
+    const { id } = req.params;
+    try {
+        const bookmark = await getBookmark(id);
+        // are you a bookmark? - this is an object, not an array. If bookmark, return it.
+        if (bookmark.id) {
+            res.status(200).json(bookmark);
+        } else {
+            res.status(500).json({ error: "Bookmark not found!" });
+        }
+    } catch (error) {
+        console.log(error);
     }
 })
 
