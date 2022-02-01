bdbaa825452c266591e394cb2d4c7d2021503333
Christina Loiacono <loiaconochristina@gmail.com>
2022-02-01 11:26:11 -0500

    created query for getting ONE bookmark

--- backend/queries/bookmarks.js
+++ backend/queries/bookmarks.js
@@ -1,14 +1,33 @@
+const { one } = require("../db/dbConfig.js");
 const db = require("../db/dbConfig.js");
 
+/* Getting ALL bookmarks */
 const getAllBookmarks = async () => {
     try {
-        const allBookmarks = await db.any("SELECT * FROM bookmarks;");
+        const allBookmarks = await db.any("SELECT * FROM bookmarks");
         return allBookmarks;
     } catch(err) {
         return err;
     }
 }
 
+/* Getting ONE bookmark */
+const getBookmark = async (id) => {
+    try {
+        /*
+         ----- About SQL injection:  -----
+        id = "1;SELECT * FROM users;"
+        if someone were to put this in your database, they would have access to all your users, which is very scary. They could potentially make changes to your database. VERY BAD. "SQL injection. So don't use interpolation in our query (like this: id=${id} )"
+        By doing it this way, it's more for security and protecting your database.
+        */
+        const oneBookmark = await  db.one("SELECT * FROM bookmarks WHERE id=$1", id);
+        return oneBookmark;
+    } catch (error) {
+        return error;
+    }
+}
+
 module.exports = {
-    getAllBookmarks
+    getAllBookmarks,
+    getBookmark
 };
