f85cff2b84891f0a04ee0a068c46b4ff609593d2
Christina Loiacono <loiaconochristina@gmail.com>
2022-02-03 11:19:51 -0500

    added axios.get request for Bookmarks.js on frontend

--- frontend/src/Components/Bookmarks.js
+++ frontend/src/Components/Bookmarks.js
@@ -6,7 +6,16 @@ const API = process.env.REACT_APP_API_URL;
 
 function Bookmarks() {
   const [bookmarks, setBookmarks] = useState([]);
-  useEffect(() => {}, []);
+  
+  useEffect(() => {
+    axios.get( API + "/bookmarks" )
+    .then((res)=>{
+      setBookmarks(res.data);
+    }).catch((err)=>{
+      console.log(err);
+    })
+  }, []);
+
   return (
     <div className="Bookmarks">
       <section>
