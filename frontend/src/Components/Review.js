122e3857c31a88cd1ddea1e15e20548348f1835c
Christina Loiacono <loiaconochristina@gmail.com>
2022-02-04 15:35:55 -0500

    adding edit form functionality. in Review.js

--- frontend/src/Components/Review.js
+++ frontend/src/Components/Review.js
@@ -1,11 +1,43 @@
-function Review({ review }) {
+import ReviewForm from "./ReviewForm";
+import { useState } from "react";
+
+/* Passing down some new props - handleDelete and handleSubmit */
+function Review({ review, handleDelete, handleSubmit }) {
+    /* Set state (boolean) */
+    const [viewEditForm, toggleEditForm] = useState(false);
+
+    /* button that toggles the edit form */
+    const toggleView = () => {
+        toggleEditForm(!viewEditForm);
+      };
+
     return (
-      <div className="Review">
-        <h4>
-          {review.title} <span>{review.rating}</span>
-        </h4>
-        <h5>{review.reviewer}</h5>
-        <p>{review.content}</p>
+        <div className="Review">
+        <button onClick={toggleView}>edit this review</button>
+
+        {/* Adding Ternary - if viewEditForm button has been clicked (boolen is true), rendering pre-filled ReviewForm, OR, all our normal information from before will be displayed. */}
+        {   
+            viewEditForm ? (
+                <ReviewForm reviewDetails={review} />
+            ) : (
+                <div>
+                <h4>
+                    {review.title} <span>{review.rating}</span>
+                </h4>
+                <h5>{review.reviewer}</h5>
+                <p>{review.content}</p>
+                </div>
+            )
+        }
+
+        <div>
+          <h4>
+            {review.title} <span>{review.rating}</span>
+          </h4>
+          <h5>{review.reviewer}</h5>
+          <p>{review.content}</p>
+          {/* Adding functionality to delete a review */}
+        </div><button onClick={() => handleDelete(review.id)}>delete</button>
       </div>
     );
   }
