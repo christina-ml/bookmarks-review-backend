21f16fcb599c15579482cd23246e1ff225e54b0f
Christina Loiacono <loiaconochristina@gmail.com>
2022-02-04 15:15:14 -0500

    Added functionality to add a new review. in Reviews.js

--- frontend/src/Components/Reviews.js
+++ frontend/src/Components/Reviews.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Review from "./Review";
+import ReviewForm from "./ReviewForm";
 
 const API = process.env.REACT_APP_API_URL;
 
@@ -15,8 +16,25 @@ function Reviews() {
       setReviews(response.data);
     });
   }, [id, API]);
+
+  const handleAdd = (newReview) => {
+    axios
+      .post(`${API}/bookmarks/${id}/reviews`, newReview)
+      .then(
+        (response) => {
+          setReviews([response.data, ...reviews]);
+        },
+        (error) => console.error(error)
+      )
+      .catch((c) => console.warn("catch", c));
+  };
+
   return (
     <section className="Reviews">
+      <h2>Reviews</h2>
+      <ReviewForm handleSubmit={handleAdd}>
+        <h3>Add a New Review</h3>
+      </ReviewForm>
       {reviews.map((review) => (
         <Review key={review.id} review={review} />
       ))}
